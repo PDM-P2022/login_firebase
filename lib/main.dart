9aa81967ebbf45d54e4310e11466b47d1b7418ef
01FC <javier.francisco01.09@gmail.com>
2022-02-23 12:41:49 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:app_firebase/login/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
